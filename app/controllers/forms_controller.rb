e70db130b13de29c09870816de7cbb95e0b3acd2
tatsuhiroshiina <tatsuhiroshiina123@gmail.com>
2022-06-16 23:09:56 +0900

    t32

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -19,6 +19,7 @@ class FormsController < ApplicationController
   def result
     @form = Form.find_by(id: params[:id])
     if @form.product_type == "ソフトウェア"
+      #省く事業
       @software = Solution.where(description: "テックイベント参加支援")
     else
       @software = nil
@@ -29,6 +30,7 @@ class FormsController < ApplicationController
       @seed = nil
     end
 
+
     if @form.series == "シリーズB" || "シリーズA"
       @seriesab = Solution.where(title: "内閣府アクセラレーションプログラム Global Preparationコース")
     else
@@ -36,37 +38,25 @@ class FormsController < ApplicationController
     end
 
     if @form.company_type == "法人向け"
-      @btob = Solution.where(description: "長期アクセラレーションプログラム").where.not(classification: "X-Hub").where.not(title: "内閣府アクセラレーションプログラム Enterpriseコース")
+      @btob = Solution.where(description: "長期アクセラレーションプログラム").where(classification: nil).where.not(title: "内閣府アクセラレーションプログラム Enterpriseコース")
     else
       @btob = nil
     end
 
-    if @form.series == "シリーズC〜"
-      @seriesc = Solution.where(title: "グローバル・アクセラレーション・ハブ").or(Solution.where.not(classification: "X-Hub").where(description: "長期アクセラレーションプログラム"))
-    else
-      @seriesc = nil
-    end
-
-    if @form.objective == "販路開拓"
-      @selling = Solution.where(title: "グローバル・アクセラレーション・ハブ")
-    else
-      @selling = nil
-    end
-
     if @form.industry == "環境・エネルギー"
-      @cleantech = Solution.where(description: "長期アクセラレーションプログラム").where.not(classification: "X-Hub").where.not(title: "内閣府アクセラレーションプログラム CleanTechコース")
+      @cleantech = Solution.where(description: "長期アクセラレーションプログラム").where(classification: nil).where.not(title: "内閣府アクセラレーションプログラム CleanTechコース")
     else
       @cleantech = nil
     end
 
     if @form.industry == "バイオ・医療"
-      @bio = Solution.where(description: "長期アクセラレーションプログラム").where.not(classification: "X-Hub").where.not(title: "内閣府アクセラレーションプログラム Bio/Healthcareコース")
+      @bio = Solution.where(description: "長期アクセラレーションプログラム").where(classification: nil).where.not(title: "内閣府アクセラレーションプログラム Bio/Healthcareコース")
     else
       @bio = nil
     end
 
     if @form.industry == "大学発ディープテック"
-      @university = Solution.where(description: "長期アクセラレーションプログラム").where.not(classification: "X-Hub").where.not(title: "内閣府アクセラレーションプログラム Universityコース")
+      @university = Solution.where(description: "長期アクセラレーションプログラム").where(classification: nil).where.not(title: "内閣府アクセラレーションプログラム Universityコース")
     else
       @university = nil
     end
@@ -77,48 +67,56 @@ class FormsController < ApplicationController
       @other = nil
     end
 
-    if !@form.region.include?("アジア")
-      @asia = Solution.where(title: "Ignite").or(Solution.where(title: "X-Hub 深センコース")).or(Solution.where(title: "X-Hub シリコンバレーコース"))
-    else
+    if @form.region.include?("アジア")
       @asia = nil
+    else
+      @asia = Solution.where(title: "Ignite").or(Solution.where(title: "X-Hub 深センコース")).or(Solution.where(title: "X-Hub シンガポールコース"))
+
     end
 
-    if !@form.region.include?("北米")
+    if @form.region.include?("北米")
+      @usa = nil
+    else
       @usa = Solution.where(title: "CES").or(Solution.where(title: "Disrupt")).or(Solution.where(title: "Collision")).
       or(Solution.where(title: "X-Hub ニューヨークコース")).or(Solution.where(title: "X-Hub シリコンバレーコース"))
-    else
-      @usa = nil
     end
 
-    if !@form.region.include?("アフリカ")
-      @africa = Solution.where(title: "Gitex Future Stars")
-    else
+    if @form.region.include?("アフリカ")
       @africa = nil
+    else
+      @africa = Solution.where(title: "Gitex Future Stars")
     end
 
-    if !@form.region.include?("欧州")
-      @europe = Solution.where(title: "X-Hub ドイツコース").or(Solution.where(title: "VIVA Tech")).or(Solution.where(title: "欧州カンファレンスコース")).or(Solution.where(title: "4 Years From Now"))
-    else
+    if @form.region.include?("欧州")
       @europe = nil
-    end
-
-    if !@form.objective.include?("協業連携") && !@form.objective.include?("販路開拓") && !@form.objective.include?("資金調達")&& !@form.objective.include?("市場調査")
-      @objective_for_acceleration = Solution.where(description: "長期アクセラレーションプログラム")
     else
-      @objective_for_acceleration = nil
+      #欧州が入っていなければ表示しない
+      @europe = Solution.where(title: "X-Hub ドイツコース").or(Solution.where(title: "VIVA Tech")).or(Solution.where(title: "欧州コース")).or(Solution.where(title: "4 Years From Now"))
     end
 
-    if !@form.region.include?("人材獲得")
+    if @form.objective.include?("人材獲得")
+      @recruitment = nil
+    else
       @recruitment = Solution.where(title: "Japan Day")
+    end
+
+    if @form.objective.include?("ビジネス戦略策定") || @form.objective.include?("人材獲得") || @form.objective.include?("販路開拓")|| @form.objective.include?("拠点設立")|| @form.objective.include?("協業連携")|| @form.objective.include?("市場調査")|| @form.objective.include?("資金調達")
+      @gah = nil
     else
-      @rectruitment = nil
+      @gah = Solution.where(title: "グローバル・アクセラレーション・ハブ")
+    end
+
+    if @form.objective.include?("ビジネス戦略策定")|| @form.objective.include?("資金調達")|| @form.objective.include?("市場調査")
+      @acceleration = nil
+    else
+      @acceleration = Solution.where(description: "長期アクセラレーションプログラム")
     end
 
 
     @solutions = Solution.where.not(id: @software).where.not(id: @seriesc).where.not(id: @cleantech).where.not(id: @bio).where.not(id: @other).where.not(id: @university).
     where.not(id: @asia).where.not(id: @usa).where.not(id: @africa).where.not(id: @europe).
-    where.not(id: @selling).where.not(id: @objective_for_acceleration).where.not(title: "始動").where.not(title: "貿易実務相談").where.not(id: @recruitment).
-    where.not(id: @seriesab).where.not(id: @btob)
+    where.not(id: @selling).where.not(id: @acceleration).where.not(title: "始動").where.not(title: "貿易投資相談").where.not(id: @recruitment).
+    where.not(id: @seriesab).where.not(id: @btob).where.not(id: @acceleration).where.not(id: @gah).where.not(id: @recruitment).order('updated_at DESC')
 
     if @form.series == "スタートアップではない"
       @solutions = Solution.where(title: "始動").or(Solution.where(title: "貿易投資相談"))
